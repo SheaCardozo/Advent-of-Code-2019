209c302cb1dbc727be2205bc1e5f96a36617d774
SheaCardozo <sacardoz@uwaterloo.ca>
2019-12-08 00:51:41 -0500

    tweaking day 8 p2 solution

--- 2019/Day 8/Q8b.py
+++ 2019/Day 8/Q8b.py
@@ -4,26 +4,18 @@ with open('Q8.txt', 'r') as op:
 prompt = prompt.strip()
 layers = []
 for i in range(0, len(prompt), 150):
-
-    layer = [0] * 150
-
-    for k, j in enumerate(prompt[i:i+150]):
-        layer[k] = int(j)
-
+    layer = prompt[i:i+150]
     layers.append(layer)
 
-image = [0] * 150
+image = ""
 
 for i in range(150):
     for layer in layers:
-        if layer[i] == 2:
-            continue
-        else:
-            image[i] = layer[i]
+        if layer[i] != '2':
+            image += layer[i]
             break
 
 for j in range(6):
-    print(image[j*25: (j+1)*25])
-    print('\n')
+    print(image[j*25: (j+1)*25].replace('0', ' '))
 
 #Printed answer is FHJUL
