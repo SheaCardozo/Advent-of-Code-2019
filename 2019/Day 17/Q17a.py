0e2edf34c9a0db74d9a9c9478ecf2df8dc396054
SheaCardozo <sacardoz@uwaterloo.ca>
2019-12-22 15:34:55 -0500

    day 17

--- 2019/Day 17/Q17a.py
+++ 2019/Day 17/Q17a.py
@@ -8,6 +8,8 @@ prompt = prompt.split(',')
 
 prompt = list(map(int, prompt))
 
+field = [[]]
+
 
 class Part17a(Intcode):
 
@@ -15,10 +17,30 @@ class Part17a(Intcode):
         return 0
 
     def _output(self, out):
-        print(out)
+        global field
+
+        out = chr(out)
+        if out == '\n':
+            field.append([])
+        else:
+            field[-1].append(out)
         return
 
 
 ic = Part17a(prompt)
 ic.run()
 
+while True:
+    if len(field[-1]) == 0:
+        field = field[:-1]
+    else:
+        break
+
+ans = 0
+
+for y in range(1, len(field) - 1):
+    for x in range(1, len(field[0]) - 1):
+        if all([p == '#' for p in (field[y][x], field[y + 1][x], field[y - 1][x], field[y][x + 1], field[y][x - 1])]):
+            ans += x * y
+
+print(ans)
